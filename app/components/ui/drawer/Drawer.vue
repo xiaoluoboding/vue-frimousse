216e04ec55f2ce5447c36eb8955147530473996b
Robert Shaw <xiaoluoboding@gmail.com>
2025-09-11 15:45:26 +0800

    fix: fixed the build

--- app/components/ui/drawer/Drawer.vue
+++ app/components/ui/drawer/Drawer.vue
@@ -3,9 +3,10 @@ import type { DrawerRootEmits, DrawerRootProps } from "vaul-vue"
 import { useForwardPropsEmits } from "reka-ui"
 import { DrawerRoot } from "vaul-vue"
 
-const props = withDefaults(defineProps<DrawerRootProps>(), {
-  shouldScaleBackground: true,
-})
+// temp fix for fadeFromIndex: https://github.com/unovue/shadcn-vue/issues/1305
+const props = withDefaults(defineProps<Omit<DrawerRootProps, 'fadeFromIndex'>>(), {
+  shouldScaleBackground: true
+});
 
 const emits = defineEmits<DrawerRootEmits>()
 
